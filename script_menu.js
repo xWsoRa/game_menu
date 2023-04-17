12593cc36d46a7368abf4bd74a1ed899d60ce58f
xWsoRa <130982325+xWsoRa@users.noreply.github.com>
2023-04-17 22:41:29 +0200

    Update script_menu.js

--- script_menu.js
+++ script_menu.js
@@ -8,5 +8,5 @@ gameButton2.addEventListener("click", function() {
 });
 const gameButton3 = document.getElementById("gameButton3");
 gameButton3.addEventListener("click", function() {
-  window.open("file:///C:/Users/Pilar/Desktop/Uni%20mario/curso%204/semantica/game%20from%20internet/long.html", "_blank");
+  window.open("https://xwsora.github.io/long_questions/", "_blank");
 });
